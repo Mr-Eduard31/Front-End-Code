12663752fdea8a8c048840702575e0f5e965b7e8
Eduard Dueñas <112318172+Mr-Eduard31@users.noreply.github.com>
2022-09-02 01:43:45 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
 
+function myOptions() {
+    var text = document.querySelector("#mySelectors");
+    text.style.color = "yellow";
+    var opt = document.getElementById("mySelectors").value;
+    document.getElementById("demo").innerHTML = opt;
+}
+function linkread() {
+    document.getElementById("pRM").innerHTML = "Web Developer is required a skills of Front end Development: Html, Css, Javascript and for Back end Development is Php, Java, Nodejs Python etc.";
+}
+function Executer() {
+  document.getElementById("myImages").src = "./image/eduard.jpg";
+}
+function myExecuter() {
+  document.getElementById("ttImg").src = "./image/eduard.jpg";
+}
